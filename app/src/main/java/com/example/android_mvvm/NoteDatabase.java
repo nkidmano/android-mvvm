5108f7dba526bfeb556470c7156a9c68dc8ecdaa
Tan <tandt@necsv-hcm.com>
2019-03-14 17:19:55 +0700

    implement add note

--- app/src/main/java/com/example/android_mvvm/NoteDatabase.java
+++ app/src/main/java/com/example/android_mvvm/NoteDatabase.java
@@ -1,25 +1,15 @@
 package com.example.android_mvvm;
 
-import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
-import android.os.AsyncTask;
-import android.support.annotation.NonNull;
 
 @Database(entities = {Note.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
 
     // Make db singleton
     private static NoteDatabase instance;
-    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
-        @Override
-        public void onCreate(@NonNull SupportSQLiteDatabase db) {
-            super.onCreate(db);
-            new PopulateDbAsyncTask(instance).execute();
-        }
-    };
 
     public static synchronized NoteDatabase getInstance(Context context) {
         // If no thread had instantiate db
@@ -27,27 +17,10 @@ public abstract class NoteDatabase extends RoomDatabase {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     NoteDatabase.class, "note_database")
                     .fallbackToDestructiveMigration()
-                    .addCallback(roomCallback)
                     .build();
         }
         return instance;
     }
 
     public abstract NoteDao noteDao();
-
-    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
-        private NoteDao noteDao;
-
-        private PopulateDbAsyncTask(NoteDatabase noteDatabase) {
-            noteDao = noteDatabase.noteDao();
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            noteDao.insert(new Note("Title 1", "Description 1", 1));
-            noteDao.insert(new Note("Title 2", "Description 2", 2));
-            noteDao.insert(new Note("Title 3", "Description 3", 3));
-            return null;
-        }
-    }
 }
